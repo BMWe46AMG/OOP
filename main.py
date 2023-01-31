5b89cea589f5f5a0011758b7a863b24dac563f96
BMWe46AMG <narek.fahradyan@mail.ru>
2023-01-31 21:37:11 +0400

    tnayin ashxatanq

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 print('Hello World')
+
+# txt = str(input('Input your text: '))
+# print(len(txt))
+# print((txt + ' ') * 10)
+# print(txt[0])
+# print(txt[:3])
+# print(txt[-3:])
+# print(txt[::-1])
